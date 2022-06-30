594633b0096702d408a7dab590ec0dd3b9d50153
ChillyYep <38913667+ChillyYep@users.noreply.github.com>
2022-06-30 19:34:16 +0800

    射线，全局光照渲染测试场景

---
+++ Assets/Script/Test/CubeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public float speed = 1f;
+    public Transform cameraTrans;
+
+    private void Update()
+    {
+        var oldPos = transform.position;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += Vector3.right * speed;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position -= Vector3.forward * speed;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position -= Vector3.right * speed;
+        }
+        if (Physics.Raycast(transform.position + Vector3.up, Vector3.down, out var hitInfo, 4f, LayerMask.GetMask("Ground")))
+        {
+            Debug.Log($"Raycast HitInfo {hitInfo}");
+            transform.position = new Vector3(transform.position.x, hitInfo.point.y + 2f, transform.position.z);
+        }
+        if (cameraTrans != null)
+        {
+            cameraTrans.transform.position += transform.position - oldPos;
+        }
+    }
+}
